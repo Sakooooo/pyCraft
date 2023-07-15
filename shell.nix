7811d554a06113fbd9746bc39033e8cbdf2b115b
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-16 00:07:16 +0300

    idont know what i did what help

--- shell.nix
+++ shell.nix
@@ -1,11 +1,14 @@
 with import <nixpkgs> {};
 
 mkShell {
+  name = "PyCraft - Nix Shell";
   nativeBuildInputs = [
     python3
   ];
   buildInputs = with python310Packages; [
     pygame
+    pyopengl
+    numpy
   ];
 
 }
