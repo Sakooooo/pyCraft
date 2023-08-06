3a52097508ef0f7379be5eee26e2e1dc9e1e93ae
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-06 18:32:08 +0300

    switch to flakes

--- flake.nix
+++ flake.nix
@@ -1,24 +1,23 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-stable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    supportedSystems = ["x86_64-linux"];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    #       ↑ Swap it for your system if needed
+    #       "aarch64-linux" / "x86_64-darwin" / "aarch64-darwin"
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
-    packages =
-      forAllSystems (system: {
-      });
-
-    devShells = forAllSystems (system: {
-      default = pkgs.${system}.mkShell {
-        packages = with pkgs.python3.withPackages; [
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        (pkgs.python3.withPackages (python-pkgs: [
           python-pkgs.pygame
-        ];
-      };
-    });
+          python-pkgs.numpy
+          python-pkgs.moderngl
+        ]))
+      ];
+      # ...
+    };
   };
 }
